b3a9df68f1104276fb71d00f5cdb26f790ff2a8b
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2025-12-17 10:11:31 -0800

    refactor: styles (#258)
    ## What Did You Change?
    
    Break up styles so "entrypoint" to contribute CSS is smaller.
    
    <details>
    
    - **Reorganized styles.**
        - Offer `inbox.css` (free-form) and `import.css` (structured).
        - Hide advanced stylesheets in subdirectories.
    - Renamed extra JavaScript file.
        <sup>(unused, empty)</sup>
    
    </details>
    
    ## Do You Want Support (syntax, design, etc)?
    
    Does this structure to make CSS approachable or more intimidating?
    
    **BEFORE**
    [One 300-line
    stylesheet](https://github.com/DesignSafe-CI/DS-User-Guide/blob/v3.0.0/user-guide/css/ds-user-guide.css)
    (organized and commented).
    ```
    .
    ├── ds-user-guide.css
    └── README.md
    ```
    
    **AFTER**
    Many small stylesheets. One [import
    stylesheet](https://github.com/DesignSafe-CI/DS-User-Guide/blob/refactor/css-and-js/user-guide/css/import.css).
    One ["inbox"
    stylesheet](https://github.com/DesignSafe-CI/DS-User-Guide/blob/refactor/css-and-js/user-guide/css/inbox.css).
    ```
    .
    ├── ds-user-guide
    │   ├── …
    │   └── …
    ├── for-other-repos
    │   ├── …
    │   └── …
    ├── tacc
    │   ├── …
    │   └── …
    ├── import.css
    ├── inbox.css
    └── README.md
    ```

---
+++ user-guide/js/import.js
@@ -0,0 +1,13 @@
+/*
+ * DesignSafe User Guide Scripts - Import Manifest
+ *
+ * IMPORTANT: Do not add scripts directly to this file.
+ * Add scripts to appropriate stylesheet or folder.
+ * If unsure where scripts belong, add them to `inbox.module.js`
+ */
+
+/* Contribute any scripts */
+import('./inbox.module.js');
+
+/* Organize established scripts */
+import('./ds-user-guide/sample.module.js');
---
+++ user-guide/js/inbox.module.js
@@ -0,0 +1,13 @@
+/*
+ * Script Contributions Inbox
+ *
+ * Any DesignSafe-specific scripting may go here.
+ * These will be periodically reviewed and organized into appropriate files.
+ *
+ * IMPORTANT: Add a comment describing what your script does.
+ */
+
+
+/* To test this script is loaded */
+// console.log('Test inbox JavaScript');
+
